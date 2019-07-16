a44eb1709968e0f4017a69b8641663de0112eef6
MdAnayat <50835349+MdAnayat@users.noreply.github.com>
2019-07-17 00:57:14 +0200

    Update writeafile.c

--- writeafile.c
+++ writeafile.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+ void main()
+ {
+     FILE *fptr;
+     char name[20];
+     int age;
+     int salary;
+     fptr =fopen("C:\\emp.rec","w");
+    if(fptr==NULL)
+    {
+        printf("File does not exist");
+        return ;
+    }
+    printf("Enter the name\n");
+    gets(name);
+    fprintf(fptr, "Name =%s\n" ,name);
+    printf("Enter the age\n");
+    scanf("%d",&age);
+    fprintf(fptr, "Age  =%d\n" ,age);
+    printf("Enter the salary\n");
+    scanf("%d",&salary);
+    fprintf(fptr, "salary  =%d\n", salary);
+    fclose(fptr);
+ }
 
