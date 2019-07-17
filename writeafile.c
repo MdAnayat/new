b0c854862d604e8b74d47609782277edb8ec5819
MdAnayat <anayatpapppu@gmail.com>
2019-07-17 02:10:47 +0200

    your commit message

--- writeafile.c
+++ writeafile.c
@@ -2,10 +2,10 @@
  void main()
  {
      FILE *fptr;
-     char name[55];
+     char name[10];
      int age;
      int salary;
-     fptr =fopen("C:\\abullllllllll.bin","w");
+     fptr =fopen("C:\\abul.bin","w");
     if(fptr==NULL)
     {
         printf("File does not exist");
